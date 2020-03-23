ee999cef59018228d62e26d91508d321efd4e476
xiaoyunfei <xiaoyunfei@credit-first.com.cn>
2020-03-23 22:14:26 +0800

    测试公钥

--- src/test/java/cn/freefly/springboot/SpringbootApplicationTests.java
+++ src/test/java/cn/freefly/springboot/SpringbootApplicationTests.java
@@ -1,5 +1,7 @@
 package cn.freefly.springboot;
 
+import cn.freefly.springboot.devUtils.DateUtil;
+import cn.freefly.springboot.devUtils.RSAUtils;
 import cn.freefly.springboot.redis.service.RedisService;
 import cn.freefly.springboot.redis.util.SequenceUtils;
 import org.junit.Test;
@@ -10,6 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,5 +30,12 @@ public class SpringbootApplicationTests {
         String flowCode = SequenceUtils.getSequence(num);
         System.out.println("流水号: " + flowCode);
     }
-
+    @Test
+    public void testPublicKey(){
+        Map<String, String> keyMap = RSAUtils.createKeys(512);
+        String  publicKey = keyMap.get("publicKey");
+        String  privateKey = keyMap.get("privateKey");
+        System.out.println("公钥: \n\r" + publicKey);
+        System.out.println("私钥： \n\r" + privateKey);
+    }
 }
