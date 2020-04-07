b4d5f37ceda1554dc70aaa12c84645d8d8e74dae
xiaoyunfei <xiaoyunfei@credit-first.com.cn>
2020-04-07 12:02:23 +0800

    FastDFS提交

---
+++ src/main/java/cn/freefly/springboot/fastDFS/config/FastDFSConfig.java
@@ -0,0 +1,19 @@
+package cn.freefly.springboot.fastDFS.config;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * @ClassNmae FastDFSConfig
+ * @Author xiao.yunfei
+ * @Date 2020/4/7 11:05
+ * @Desc
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastDFSConfig {
+}
