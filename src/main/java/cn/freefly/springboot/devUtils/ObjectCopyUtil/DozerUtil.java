22203db87b8489a2f3b6761b1cfe795c81d36f39
xiaoyfc <1226781445@qq.com>
2020-04-09 22:38:52 +0800

    dozer提交

--- src/main/java/cn/freefly/springboot/devUtils/ObjectCopyUtil/DozerUtil.java
+++ src/main/java/cn/freefly/springboot/devUtils/ObjectCopyUtil/DozerUtil.java
@@ -18,13 +18,19 @@ import java.util.stream.Collectors;
 import static com.github.dozermapper.core.loader.api.TypeMappingOptions.mapNull;
 
 /**
- * @ClassNmae BeanMapperUtil
+ * @ClassNmae DozerUtil
  * @Author xiao.yunfei
  * @Date 2020/3/29 23:43
  * @Desc
  */
-public class BeanMapperUtil {
-
+public class DozerUtil {
+    /**
+     * @Author: xiao.yunfei
+     * @Description: API 自定义 mapper 避免源对象空值属性覆盖目标对象值未匹配字段数据
+     * @Date: 19:58 2020/4/9
+     * @Param: [sources, destination]
+     * @Return: void
+     */
     public static <T, K> void copyPropertiesWithNull(final T sources, final K destination) {
         Mapper mapper = DozerBeanMapperBuilder.create().withMappingBuilder(new BeanMappingBuilder() {
             @Override
