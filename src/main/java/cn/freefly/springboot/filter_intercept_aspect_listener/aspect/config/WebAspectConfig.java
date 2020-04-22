82975d812b75719ad07b4ae37031f9ff6ce3374f
xiaoyunfei <xiaoyunfei@credit-first.com.cn>
2020-04-22 18:46:33 +0800

    切面

---
+++ src/main/java/cn/freefly/springboot/filter_intercept_aspect_listener/aspect/config/WebAspectConfig.java
@@ -0,0 +1,16 @@
+package cn.freefly.springboot.filter_intercept_aspect_listener.aspect.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassNmae WebAspectConfig
+ * @Author xiao.yunfei
+ * @Date 2020/4/22 18:14
+ * @Desc
+ */
+@Configuration
+public class WebAspectConfig {
+    public void test(){
+
+    }
+}
